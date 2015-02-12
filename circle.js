a982efb7a6ced8e6cc8317a3c29f5e91916d92ae
suisho <suisho000000@gmail.com>
2015-02-12 23:25:54 +0900

    add css

--- circle.js
+++ circle.js
@@ -2,11 +2,22 @@ function setProgress($element, progress){
   var rotation = Math.floor(progress/100 * 180);
 	var fillRotation = rotation;
 	var fixRotation = rotation * 2;
-	$element.find('.circle .fill, .circle .mask.full').css("transform", 'rotate(' + fillRotation + 'deg)');
-	$element.find('.circle .fill.right').css("transform", 'rotate(' + fixRotation + 'deg)');
-	$element.find('.timer').text(progress)
+	var length = 10 // secound
+	// css animation
+	$element.find('.fill, .mask').css("transition", 'transform '+length + "s");
+	$element.find('.fill, .mask.full').css("transform", 'rotate(' + fillRotation + 'deg)');
+	$element.find('.fill.fix').css("transform", 'rotate(' + fixRotation + 'deg)');
+
+	// text animation
+	var current = progress
+	$({count: 0}).animate({count : progress}, {
+		duration : length * 1000,
+		progress : function(){
+			$element.find('.timer').text(Math.ceil(this.count))
+		}
+	})
 }
 
 setTimeout(function(){
-	setProgress($(".radial-progress"), 30)
+	setProgress($(".radial-progress"), 100)
 }, 0)
