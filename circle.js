69c458fb369d247cd6b273a8293785c2b58f0269
suisho <suisho000000@gmail.com>
2015-02-12 22:38:40 +0900

    wip

--- circle.js
+++ circle.js
@@ -1,4 +1,12 @@
-function circleTimer($element, progress){
-  $(".circle")
+function setProgress($element, progress){
+  // 右回転
+  var lPie = $element.find(".pie.left")
+  var rPie = $element.find(".pie.right")
+  var rPieMax = 180
+  var rDeg = 0
+  if(progress < 50){
+    rDeg = rPieMax * progress/50
+  }
+  rPie.css("transform", "rotate("+rDeg+"deg)")
 }
 setProgress($(".circle-container"), 33)
